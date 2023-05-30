4fba7883ddeed665b0f0af62841e31208ac932c2
KarinaShafa <41880706+KarinaShafa@users.noreply.github.com>
2023-05-30 23:14:17 +0700

    Add files via upload

---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // memanggilan metode insert pada DB facade untuk menyisipkan data ke dalam tabel 'employees'.
+        DB::table('employees')->insert([
+            [
+                'firstname' => 'Purnama',
+                'lastname' => 'Anaking',
+                'email'=> 'purnama.anaking@gmail.com',
+                'age' => 20,
+                'position_id' => 1
+            ],
+            [
+                'firstname' => 'Adzanil',
+                'lastname' => 'Rachmadhi',
+                'email'=> 'adzanil.rachmadhi@gmail.com',
+                'age' => 25,
+                'position_id' => 2
+            ],
+            [
+                'firstname' => 'Berlian',
+                'lastname' => 'Rahmy',
+                'email'=> 'berlian.rahmy@gmail.com',
+                'age' => 23,
+                'position_id' => 3
+            ],
+        ]);
+    }
+}
